3b26ad900cbbe4df07ef2a08a72b97b42ca7d341
MikhailSK <46277377+MikhailSK@users.noreply.github.com>
2020-01-12 23:45:03 +0300

    move gan generate to thread
    poorly implemented due to the fact that I did not understand how to use the loaded model in the stream, not loading it every time again

--- gan.py
+++ gan.py
@@ -121,59 +121,6 @@ class GAN:
 
         return Model(img, validity)
 
-    def train(self, epochs, batch_size=128, save_images=10, save_model=2000):
-
-        X_train = np.array(images)
-        X_train = (X_train.astype(np.float32) - 127.5) / 127.5
-
-        half_batch = int(batch_size / 2)
-
-        mean_d_loss = [0, 0]
-        mean_g_loss = 0
-
-        for epoch in range(epochs):
-            idx = np.random.randint(0, X_train.shape[0], half_batch)
-            imgs = X_train[idx]
-
-            noise = np.random.normal(0, 1, (half_batch, self.noise_size))
-            gen_imgs = self.generator.predict(noise)
-
-            # Training the discriminator
-
-            # The loss of the discriminator is the mean of the losses while training on authentic and fake images
-            d_loss = 0.5 * np.add(self.discriminator.train_on_batch(imgs, np.ones((half_batch, 1))),
-                                  self.discriminator.train_on_batch(gen_imgs, np.zeros((half_batch, 1))))
-
-            # Training the generator
-
-            noise = np.random.normal(0, 1, (batch_size, self.noise_size))
-
-            valid_y = np.array([1] * batch_size)
-            g_loss = self.combined.train_on_batch(noise, valid_y)
-
-            mean_d_loss[0] += d_loss[0]
-            mean_d_loss[1] += d_loss[1]
-            mean_g_loss += g_loss
-
-            # We print the losses and accuracy of the networks every 10
-            # batches mainly to make surethe accuracy of the discriminator
-            # is not stable at around 50% or 100% (which would mean
-            # the discriminator performs not well enough or too well)
-            if epoch % 1 == 0:
-                print("%d [Discriminator loss: %f, acc.: %.2f%%] [Generator loss: %f]" %
-                      (epoch, mean_d_loss[0] / 200, mean_d_loss[1] / 2, mean_g_loss / 200))
-
-                mean_d_loss = [0, 0]
-                mean_g_loss = 0
-
-            if epoch % save_images == 0:
-                self.save_images(epoch)
-
-            # We save the architecture of the model, the weights and the state of the optimizer
-            # This way we can restart the training exactly where we stopped
-            if epoch % save_model == 0:
-                self.save_model(epoch)
-
     # Saving n generated images to have a representation of the spectrum of images created by the generator
     def save_images(self, epoch):
         noise = np.random.normal(0, 1, (25, self.noise_size))
--- qt_gan.py
+++ qt_gan.py
@@ -1,23 +1,33 @@
 import sys
 
 from PyQt5 import uic
-from PyQt5.QtCore import QThread
+from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPixmap
 from gan import GAN
 
 
 class Loader(QThread):
-    def __init__(self):
+    change_value = pyqtSignal(int)
+
+    def __init__(self, par):
         super().__init__()
         self.generator = "generator_4500.h5"
         self.discriminator = "discriminator_4500.h5"
+        self.par = par
+        self.gan = None
+
+    def get_gan(self):
+        return self.gan
 
     def run(self):
-        gan = GAN()
-        gan.load_model(self.generator, self.discriminator)
+        if self.par == 0:
+            self.gan = GAN()
+            self.gan.load_model(self.generator, self.discriminator)
+            self.par += 1
         i = 0
-        gan.save_images(i)
+        self.gan.save_images(i)
+        self.change_value.emit(i)
 
 
 class GANWidget(QMainWindow):
@@ -25,26 +35,24 @@ class GANWidget(QMainWindow):
         super().__init__()
         self.title = "GAN"
         self.i = 0
-        self.InitUI()
-        load = Loader()
-        load.change_value.connect(self.put_image())
-        load.start()
-        self.pixmap = QPixmap('images/image_' + str(self.i) + '.png')
+        self.load = Loader(0)
+        self.load.start()
         self.InitUI()
 
     def InitUI(self):
         uic.loadUi('ui/gan.ui', self)
         self.btn_generate.clicked.connect(
             self.btn_generate_onClick)
+        self.put_image()
         self.show()
 
     def btn_generate_onClick(self):
-        self.i += 1
-        self.gan.save_images(self.i)
-        self.pixmap = QPixmap('images/image_' + str(self.i) + '.png')
+        self.load = Loader(0)
+        self.load.start()
         self.put_image()
 
     def put_image(self):
+        self.pixmap = QPixmap('images/image_' + str(self.i) + '.png')
         self.img.setPixmap(self.pixmap)
 
     @staticmethod
